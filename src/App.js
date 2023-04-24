3a4476eb0fa63051a0c2f61d358e78e8cb15e976
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-04-24 19:33:48 +0200

    create a cool animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/loginPage/LoginPage";
+import Animation from "./components/animation/Animation";
 import HomePage from "./components/homePage/HomePage";
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/animation" element={<Animation />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/animation/Animation.jsx
@@ -0,0 +1,77 @@
+import "./animation.css";
+
+const Animation = () => {
+  return (
+    <div className="animation-bigger">
+      <div className="animation-body">
+        <div className="container">
+          <div className="item" style={{ "--i": 0 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 1 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 2 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 3 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 4 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 5 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 6 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 7 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 8 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 9 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 10 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 11 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 12 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 13 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 14 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 15 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 16 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 17 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 18 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 19 }}>
+            <i className="fas fa-music"></i>
+          </div>
+          <div className="item" style={{ "--i": 20 }}>
+            <i className="fas fa-music"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
